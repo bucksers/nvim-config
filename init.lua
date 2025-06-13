94927e21e568eeba60299134800ca42d08b9a235
Will Buckser-Schulz <bucksers@usc.edu>
2025-06-12 19:36:32 -0700

    branch isolation for plugins in lazyvim branch

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
+-- Setup branch-based plugin isolation BEFORE loading any plugins
+require("config.branch-isolation").setup()
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
