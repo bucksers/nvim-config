3b8dabf8fde7bfb0be8f031f1b912325f470ddba
Will Buckser-Schulz <bucksers@usc.edu>
2025-06-24 22:07:44 -0700

    lazyvim 6/24/25

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable gray highlighting for unused/unnecessary code from LSP diagnostics
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    vim.schedule(function()
+      vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "NONE", bg = "NONE" })
+    end)
+  end,
+})
